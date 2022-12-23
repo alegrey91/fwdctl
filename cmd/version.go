1d237e2ef952e97a2909a67138309c153975a613
alegrey91 <ale_grey_91@hotmail.it>
2022-12-23 22:01:50 +0100

    fix: print tag version without 'v' char

--- cmd/version.go
+++ cmd/version.go
@@ -33,7 +33,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("v%s\n", c.Version)
+		fmt.Printf("%s\n", c.Version)
 	},
 }
 
