951c12b7bed03353a064fa71b4fd53402573439c
Alessio Greggi <ale_grey_91@hotmail.it>
2024-03-19 19:22:11 +0100

    fix: improve generate command
    Signed-off-by: Alessio Greggi <ale_grey_91@hotmail.it>

--- pkg/template/template.go
+++ pkg/template/template.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 	"text/template"
 )
 
@@ -14,27 +15,30 @@ type Generator interface {
 	GetFileName() string
 }
 
-func GenerateTemplate(g Generator, destinationPath string) error {
+func GenerateTemplate(g Generator, outputPath string) error {
 	tpl, err := template.New(g.GetTemplateName()).Parse(g.GetFileContent())
 	if err != nil {
-		return err
+		return fmt.Errorf("error getting template instance: %v", err)
 	}
 
-	if destinationPath == "" {
-		return nil
+	if !filepath.IsAbs(outputPath) {
+		return fmt.Errorf("output path is not absolute: %s", outputPath)
 	}
-	if !filepath.IsAbs(destinationPath){
-		return fmt.Errorf("destinationPath path is not absolute")
+	// if last char of outputPath is "/" we want to remove,
+	// so the final output will be cleaned.
+	// this way: /root/template.file instead of /root//template.file
+	if outputPath != "/" && outputPath[len(outputPath)-1:] == "/" {
+		outputPath = strings.TrimSuffix(outputPath, "/")
 	}
 
-	outFile, err := os.Create(destinationPath + "/" + g.GetFileName())
+	outFile, err := os.Create(filepath.Join(outputPath, g.GetFileName()))
 	if err != nil {
-		return err
+		return fmt.Errorf("error creating output file: %v", err)
 	}
 
 	err = tpl.Execute(outFile, g.GetTemplateStruct())
 	if err != nil {
-		return err
+		return fmt.Errorf("error writing content into file: %v", err)
 	}
 	return nil
 }
