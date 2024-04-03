fba87094710b2f16751996356c99f45c95710c37
Alessio Greggi <ale_grey_91@hotmail.it>
2024-04-03 13:10:55 +0200

    test(pkg/iptables): add unit test for rule file
    Signed-off-by: Alessio Greggi <ale_grey_91@hotmail.it>

--- pkg/iptables/rule_test.go
+++ pkg/iptables/rule_test.go
@@ -55,3 +55,58 @@ func TestExtractRuleInfo(t *testing.T) {
 		})
 	}
 }
+
+func TestRule_String(t *testing.T) {
+	type fields struct {
+		Iface   string
+		Proto   string
+		Dport   int
+		Saddr   string
+		Sport   int
+		Comment string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   []string
+	}{
+		// TODO: Add test cases.
+		{
+			name: "should_return_consistent_value",
+			fields: fields{
+				Iface: "eth0",
+				Proto: "tcp",
+				Dport: 8080,
+				Saddr: "127.0.0.1",
+				Sport: 80,
+			},
+			want: []string{"-i", "eth0", "-p", "tcp", "-m", "tcp", "--dport", "8080", "-m", "comment", "--comment", "fwdctl", "-j", "DNAT", "--to-destination", "127.0.0.1:80"},
+		},
+		{
+			name: "should_return_consistent_value",
+			fields: fields{
+				Iface: "lo",
+				Proto: "tcp",
+				Dport: 8080,
+				Saddr: "127.0.0.1",
+				Sport: 90,
+			},
+			want: []string{"-i", "lo", "-p", "tcp", "-m", "tcp", "--dport", "8080", "-m", "comment", "--comment", "fwdctl", "-j", "DNAT", "--to-destination", "127.0.0.1:90"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rule := &Rule{
+				Iface:   tt.fields.Iface,
+				Proto:   tt.fields.Proto,
+				Dport:   tt.fields.Dport,
+				Saddr:   tt.fields.Saddr,
+				Sport:   tt.fields.Sport,
+				Comment: tt.fields.Comment,
+			}
+			if got := rule.String(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Rule.String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
