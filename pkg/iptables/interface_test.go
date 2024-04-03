0960d7dd436f886565016d2bb189eba38a188ac4
Alessio Greggi <ale_grey_91@hotmail.it>
2024-04-03 13:02:42 +0200

    test(pkg/iptables): add unit test for interface file
    Signed-off-by: Alessio Greggi <ale_grey_91@hotmail.it>

---
+++ pkg/iptables/interface_test.go
@@ -0,0 +1,45 @@
+package iptables
+
+import "testing"
+
+func Test_interfaceExists(t *testing.T) {
+	type args struct {
+		iface string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    bool
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "should_exist",
+			args: args{
+				iface: "lo",
+			},
+			want:    true,
+			wantErr: false,
+		},
+		{
+			name: "should_not_exist",
+			args: args{
+				iface: "xyz0",
+			},
+			want:    false,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := interfaceExists(tt.args.iface)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("interfaceExists() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("interfaceExists() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
