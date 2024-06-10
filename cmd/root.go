7104727b13671a657afcf3c70600138095c02b9f
Alessio Greggi <ale_grey_91@hotmail.it>
2024-06-10 10:26:56 +0200

    style: update general command description
    Signed-off-by: Alessio Greggi <ale_grey_91@hotmail.it>

--- cmd/root.go
+++ cmd/root.go
@@ -17,20 +17,16 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/spf13/cobra"
 	"os"
+
+	"github.com/spf13/cobra"
 )
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "fwdctl",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "fwdctl is a simple and intuitive CLI to manage IPTables forwards",
+	Long:  ``,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
