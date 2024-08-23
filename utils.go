34068de4ea8a71077482c6d5884a9d95dce890bb
Alessio Greggi <ale_grey_91@hotmail.it>
2024-08-23 18:29:19 +0200

    add harpoon in pipeline
    Signed-off-by: Alessio Greggi <ale_grey_91@hotmail.it>

--- utils.go
+++ utils.go
@@ -1,19 +1,9 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
-	"fmt"
-	"os"
-	"os/exec"
-	"path/filepath"
-	"regexp"
-	"time"
-
 	"github.com/alegrey91/fwdctl/pkg/iptables"
 	goiptables "github.com/coreos/go-iptables/iptables"
 	"github.com/rogpeppe/go-internal/testscript"
-	"github.com/u-root/u-root/pkg/strace"
 )
 
 func fwdExists(ts *testscript.TestScript, neg bool, args []string) {
@@ -49,103 +39,11 @@ func fwdExists(ts *testscript.TestScript, neg bool, args []string) {
 	}
 }
 
-func straceCmd(ts *testscript.TestScript, neg bool, args []string) {
-	if len(args) < 1 {
-		ts.Fatalf("syntax: strace needs at least one argument")
-	}
-
-	outputDir := "syscalls"
-	if err := os.MkdirAll(outputDir, 0755); err != nil {
-		ts.Fatalf("failed to create output directory: %v", err)
-	}
-
-	// Prepare the strace output file
-	timestamp := time.Now().Format("20060102-150405")
-	outputFile := filepath.Join(outputDir, fmt.Sprintf("strace-%s.log", timestamp))
-
-	// Prepare the command to execute with strace, filtering only system calls
-	tracedCmd := exec.Command(args[0], args[1:]...)
-
-	var stdoutBuf bytes.Buffer
-	tracedCmd.Stdout = &stdoutBuf
-	var stderrBuf bytes.Buffer
-	tracedCmd.Stderr = &stderrBuf
-
-	// Run the command
-	writer := new(bytes.Buffer)
-	if err := strace.Strace(tracedCmd, writer); err != nil {
-		if !neg {
-			ts.Fatalf("command failed: %v", err)
-		}
-	} else {
-		if neg {
-			ts.Fatalf("command succeeded when it should have failed")
-		}
-	}
-
-	fmt.Fprintf(ts.Stdout(), "%s", stdoutBuf.String())
-
-	syscalls, err := processStraceOutput(writer.String())
-	if err != nil {
-		ts.Fatalf("error processing strace output: %v", err)
-	}
-
-	err = writeToFile(outputFile, syscalls)
-	if err != nil {
-		ts.Fatalf("error creating file: %v", err)
-	}
-
-	ts.Logf("strace output saved to %s", outputFile)
-}
-
-func writeToFile(outputFile string, syscalls []string) error {
-	file, err := os.OpenFile(outputFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-
-	if err != nil {
-		return fmt.Errorf("failed creating file: %s", err)
-	}
-
-	datawriter := bufio.NewWriter(file)
-
-	for _, data := range syscalls {
-		_, _ = datawriter.WriteString(data + "\n")
-	}
-	datawriter.Flush()
-	file.Close()
-	return nil
-}
-
-func processStraceOutput(straceOutput string) ([]string, error) {
-	// Regular expression to match system calls in the format "[pid xxx] E syscall("
-	re := regexp.MustCompile(`\[pid \d+\] E (\w+)\(`)
-
-	// Find all matches of the pattern in the strace output
-	matches := re.FindAllStringSubmatch(straceOutput, -1)
-
-	// Use a map to keep track of unique system calls
-	systemCalls := make(map[string]struct{})
-
-	for _, match := range matches {
-		if len(match) > 1 {
-			systemCalls[match[1]] = struct{}{}
-		}
-	}
-
-	// Convert map keys to a slice
-	var result []string
-	for call := range systemCalls {
-		result = append(result, call)
-	}
-
-	return result, nil
-}
-
 func customCommands() map[string]func(ts *testscript.TestScript, neg bool, args []string) {
 	return map[string]func(ts *testscript.TestScript, neg bool, args []string){
 
 		// fwd_exists check that the given forward exists
 		// invoke as "fwd_exists iface proto dest_port src_addr src_port"
 		"fwd_exists": fwdExists,
-		"strace":     straceCmd,
 	}
 }
