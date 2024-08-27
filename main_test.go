cbfb7b2eec37ae5d232136564814ac04a98c23e4
Alessio Greggi <ale_grey_91@hotmail.it>
2024-08-28 00:17:04 +0200

    tracing with harpoon
    Signed-off-by: Alessio Greggi <ale_grey_91@hotmail.it>

--- main_test.go
+++ main_test.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"flag"
 	"testing"
 
 	"github.com/rogpeppe/go-internal/testscript"
 )
 
+var trace bool
+
+func init() {
+	flag.BoolVar(&trace, "tracing", false, "Trace syscalls from integration-tests")
+}
+
 func TestFwdctl(t *testing.T) {
+	flag.Parse()
 	testscript.Run(t, testscript.Params{
 		Dir:                 "tests",
 		Cmds:                customCommands(),
 		RequireExplicitExec: true,
 		Setup: func(env *testscript.Env) error {
 			env.Setenv("GOCOVERDIR", "/tmp/integration")
+			if trace {
+				env.Setenv("TRACING", "true")
+			}
 			return nil
 		},
 	})
--- utils.go
+++ utils.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
 	"os"
 	"os/exec"
@@ -46,6 +48,51 @@ func fwdExists(ts *testscript.TestScript, neg bool, args []string) {
 	if !exists {
 		ts.Fatalf("forward doesn't exist")
 	}
+}
+
+func execCmd(ts *testscript.TestScript, neg bool, args []string) {
+	tracing := ts.Getenv("TRACING")
+	var err error
+	if tracing != "true" {
+		ts.Logf("executing command: %s", strings.Join(args, " "))
+		err = ts.Exec(args[0], args[1:]...)
+	} else {
+		//uuid := getRandomString()
+		customCommand := []string{
+			"/usr/local/bin/harpoon",
+			"capture",
+			"-f",
+			"main.main",
+			"--save",
+			"--directory",
+			"integration-test-syscalls",
+			"--include-cmd-output",
+			//"--name",
+			//fmt.Sprintf("main_main_%s", uuid),
+			"--",
+		}
+		customCommand = append(customCommand, args...)
+		fmt.Println(customCommand)
+		ts.Logf("executing tracing command: %s", strings.Join(customCommand, " "))
+		err = ts.Exec(customCommand[0], customCommand[1:]...)
+	}
+	if err != nil {
+		if neg {
+			ts.Logf("expected error: %v", err)
+			return
+		}
+		ts.Fatalf("error: %v", err)
+	}
+}
+
+func getRandomString() string {
+	b := make([]byte, 4) // 4 bytes will give us 6 base64 characters
+	_, err := rand.Read(b)
+	if err != nil {
+		return ""
+	}
+	randomString := base64.URLEncoding.EncodeToString(b)[:6]
+	return randomString
 }
 
 func cmdTraceSyscalls(ts *testscript.TestScript, neg bool, args []string) {
@@ -169,7 +216,7 @@ func customCommands() map[string]func(ts *testscript.TestScript, neg bool, args 
 
 		// fwd_exists check that the given forward exists
 		// invoke as "fwd_exists iface proto dest_port src_addr src_port"
-		"fwd_exists":    fwdExists,
-		"tracesyscalls": cmdTraceSyscalls,
+		"fwd_exists": fwdExists,
+		"exec_cmd":   execCmd,
 	}
 }
