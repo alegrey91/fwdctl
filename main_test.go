3bdbc7b0600a7d2b561b63444136e5f931f1da39
Alessio Greggi <ale_grey_91@hotmail.it>
2024-09-06 17:17:17 +0200

    increase wait time before check for forwards
    Signed-off-by: Alessio Greggi <ale_grey_91@hotmail.it>

--- main_test.go
+++ main_test.go
@@ -1,22 +1,12 @@
 package main
 
 import (
-	"flag"
-	"os"
 	"testing"
 
 	"github.com/rogpeppe/go-internal/testscript"
 )
 
-var tracing *bool
-
-func init() {
-	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-	tracing = flag.Bool("tracing", false, "Trace syscalls during tests")
-}
-
 func TestFwdctl(t *testing.T) {
-	flag.Parse()
 	testscript.Run(t, testscript.Params{
 		Dir:                 "tests",
 		Cmds:                customCommands(),
